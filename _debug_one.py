ce761765658bfbd8e65611b9c3e01dd21f93bf79
haijunwu007007 <haijunwu007007@163.com>
2026-07-12 15:43:07 +0800

    cleanup: remove all temporary fix scripts and backups

--- _debug_one.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.stdout.reconfigure(encoding='utf-8')
-import ast
-
-lines=open('modules/openhands_agent.py','r',encoding='utf-8').readlines()
-for i in range(38,48):
-    print(f'{i+1}: {repr(lines[i].rstrip())}')
--- _scan_cross.py
+++
@@ -1,7 +0,0 @@
-lines = open('modules/auto_summary.py', 'r', encoding='utf-8').readlines()
-for i in range(len(lines) - 1):
-    a = lines[i].rstrip()
-    b = lines[i + 1].strip()
-    if a.endswith('"') and b.startswith('"') and not a.endswith('"""'):
-        print(f'L{i+1}: {a[-40:]}', end='')
-        print(f' / {b[:40]}')
